c0a187fe88063408a3b1661693242483fabdd41c
kotelok300 <150916406+kotelok300@users.noreply.github.com>
2023-12-20 16:15:27 +0300

    Update weather_forecast.py

--- weather_forecast.py
+++ weather_forecast.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('6437774582:AAEuxPT2j7Jt9llXKybvxC3lTmTXYuK3SvI')
-API = 'cb7c19e567c351967e0c8a47f3fa111b'
+bot = telebot.TeleBot('bot_token')
+API = 'your_API'
 
 @bot.message_handler(content_types=['text'])
 def future_weather(message):
